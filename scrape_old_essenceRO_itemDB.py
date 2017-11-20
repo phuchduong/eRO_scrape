0484a567b6ca87be9384e499526afcb287230732
Phuc H Duong <phuchduong@hotmail.com>
2017-11-19 19:48:23 -0700

    scraped the entire db, fixed bugs that stop script from scraping

--- scrape_old_essenceRO_itemDB.py
+++ scrape_old_essenceRO_itemDB.py
@@ -25,43 +25,45 @@ def scrape_page(items_list, driver):
     # isolates the div housing the item tables
     query_divs = driver.find_elements_by_id("ajax")
 
-    # grabs a list of WebElements holding the item-id and display name
-    titles = query_divs[0].find_elements_by_css_selector('h3.table-head')
+    try:
+        # grabs a list of WebElements holding the item-id and display name
+        titles = query_divs[0].find_elements_by_css_selector('h3.table-head')
 
-    # grabs a list of WebElements holding the item details
-    i_tables = query_divs[0].find_elements_by_tag_name("tbody")
+        # grabs a list of WebElements holding the item details
+        i_tables = query_divs[0].find_elements_by_tag_name("tbody")
 
-    # parse item_id and display_name
-    for i in range(0, len(titles)):
-        # loops through each title, skipping the first one.
-        id_name = titles[i].text
-        print("Found... " + id_name)
+        # parse item_id and display_name
+        for i in range(0, len(titles)):
+            # loops through each title, skipping the first one.
+            id_name = titles[i].text
+            print("Found... " + id_name)
 
-        # separates item_id from item display name
-        sep = id_name.find(" ")  # finds the first space separation
-        item_id = id_name[:sep]
-        display_name = id_name[(sep + 1):]
+            # separates item_id from item display name
+            sep = id_name.find(" ")  # finds the first space separation
+            item_id = id_name[:sep]
+            display_name = id_name[(sep + 1):]
 
-        item_entry = {
-            "item_id": item_id,
-            "display_name": display_name
-        }
+            item_entry = {
+                "item_id": item_id,
+                "display_name": display_name
+            }
 
-        # parses item details like descriptions and item types
-        col_headers = i_tables[i].find_elements_by_tag_name("th")
-        col_values = i_tables[i].find_elements_by_tag_name("td")
-        for j in range(1, len(col_headers)):
-            col_name = col_headers[j].text
-            value = col_values[j].text
+            # parses item details like descriptions and item types
+            col_headers = i_tables[i].find_elements_by_tag_name("th")
+            col_values = i_tables[i].find_elements_by_tag_name("td")
+            for j in range(1, len(col_headers)):
+                col_name = col_headers[j].text
+                value = col_values[j].text
 
-            # scripts were sanitized, putting curly brace back in
-            if(col_name == "Item Script"):
-                value = "{ " + value + " }"
+                # scripts were sanitized, putting curly brace back in
+                if(col_name == "Item Script"):
+                    value = "{ " + value + " }"
+                value = value.replace("\n", " ")  # remove new lines
+                item_entry[col_name] = value
 
-            item_entry[col_name] = value
-
-        parsed_items.append(item_entry)
-
+            parsed_items.append(item_entry)
+    except IndexError:
+        pass
     return parsed_items
 
 
@@ -77,7 +79,7 @@ def scrape_all_pages(items_list, driver):
         next_button.click()
         # recursion: scrape page
         print("Waiting for page to load...")
-        sleep(2)
+        sleep(1)
         try:
             items_list = scrape_all_pages(items_list, driver)
         except StaleElementReferenceException as e:
@@ -119,7 +121,7 @@ browser = webdriver.Chrome(executable_path=chrome_driver_path)
 parsed_items = []
 
 # url param: letter
-for c in ascii_lowercase:
+for c in "opqrstuvwxyz":
     ''' scrapes all items a-z pages '''
     p_letter = c
     target_url = base_url + "?letter=" + p_letter + "&type=" + p_type + "&limit=25&start=0"
@@ -129,45 +131,45 @@ for c in ascii_lowercase:
 
     parsed_items = scrape_all_pages(items_list=parsed_items, driver=browser)
 
+    # Gets current time now as a string
+    scrape_time = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # prints the items found and its details
+    all_headers = []
+
+    # creates a list of all possible headers by going through
+    # and recording all items and all their dictionary keys
+    for item in parsed_items:
+        for key in item:
+            if key not in all_headers:
+                all_headers.append(key)
+
+    filename = "items_" + c + "_" + scrape_time + ".tsv"
+    f = open(filename, "w")
+
+    # writes the header file
+    header = "\t".join(all_headers) + "\n"
+    f.write(header)
+
+    # writes each item into a row in the tsv
+    for item in parsed_items:
+        entry = ""
+        for header in all_headers:
+            try:
+                value = item[header]
+                entry += value
+            except KeyError:
+                pass
+            entry += "\t"  # adds a tab
+        # trims the last tab
+        last_tab = entry.rfind("\t")
+        entry = entry[:last_tab]
+        entry += "\n"
+        entry = entry.replace("�", "-")
+        f.write(entry)
+
+    # closes the file
+    f.close()
+
 # closes the browser
 browser.quit()
-
-# Gets current time now as a string
-scrape_time = datetime.now().strftime("%Y%m%d%H%M%S")
-
-# prints the items found and its details
-all_headers = []
-
-# creates a list of all possible headers by going through
-# and recording all items and all their dictionary keys
-for item in parsed_items:
-    for key in item:
-        if key not in all_headers:
-            all_headers.append(key)
-
-filename = "items_" + scrape_time + ".tsv"
-f = open(filename, "w")
-
-# writes the header file
-header = "\t".join(all_headers) + "\n"
-f.write(header)
-
-# writes each item into a row in the tsv
-for item in parsed_items:
-    entry = ""
-    for header in all_headers:
-        try:
-            value = item[header]
-            entry += value
-        except KeyError:
-            pass
-        entry += "\t"  # adds a tab
-    # trims the last tab
-    last_tab = entry.rfind("\t")
-    entry = entry[:last_tab]
-    entry += "\n"
-    entry = entry.replace("�", "-")
-    f.write(entry)
-
-# closes the file
-f.close()
