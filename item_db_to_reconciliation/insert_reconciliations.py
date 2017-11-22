814e8d026d555d5f270de1dd2c220ce336cb1c13
Phuc H Duong <phuchduong@hotmail.com>
2017-11-22 15:47:14 -0700

    finished debugging reconciliation script

--- item_db_to_reconciliation/insert_reconciliations.py
+++ item_db_to_reconciliation/insert_reconciliations.py
@@ -11,8 +11,10 @@
 '''
 import openpyxl  # excel plugin
 
+out_path = "C:/repos/eRODev/rAthena Files/db/import/ero_item_db"
+
 # extracts data from the spreadsheet
-sheet_path = "D:/repos/essencero_restoration/item_db_to_reconciliation/reconciliation.xlsx"
+sheet_path = "C:/repos/essencero_restoration/item_db_to_reconciliation/reconciliation.xlsx"
 
 excel = openpyxl.load_workbook(filename=sheet_path, data_only=True)
 
@@ -33,22 +35,33 @@ for cell in column:
     except IndexError:
         pass
 
-out_path = "D:/repos/eRODev/rAthena Files/db/import/ero_item_db"
 
 old_filename = "item_db.txt"
+old_path = out_path + "/" + old_filename
+print("Opening: " + old_path)
+old_f = open(file=old_path, mode='r')
 
-old_f = open(filename=out_path + "/" + old_filename, mode='r')
-
-new_f = open(filename="item_db_new.txt", mode="w+")
+new_filename = "item_db_new.txt"
+new_path = out_path + "/" + new_filename
+print("Opening: " + new_path)
+new_f = open(file=new_path, mode="w+")
 
 for line in old_f:
     line_split = line.split(",")
     if(len(line_split) == 22):
         item_id = line_split[0]
         if(item_id[:2] != "//" and item_id in new_entry):
+            entry = new_entry.pop(item_id)
             # If not commented out and id matches the new entry insert
-            print("Inserting... " + item_id + row[5:15])
-            new_f.write(new_entry.pop(item_id))
+            entry_length = len(entry.split(","))
+            if(entry_length>=22):
+                print("Inserting... " + item_id + entry[5:15])
+                new_f.write(entry + "\n")
+            else:
+                print("Bad number of columns for " + item_id + ". Columns: " + entry_length)
+        else:
+            # If the line is not referenced in the file, write it.
+            new_f.write(line)
     else:
         # If the line is not referenced in the file, write it.
         new_f.write(line)
