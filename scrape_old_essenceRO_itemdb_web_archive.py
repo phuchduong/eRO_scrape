db408d7f71cb3f7ba07a5cc39a59f424287f0b13
Phuc H Duong <phuchduong@hotmail.com>
2017-11-22 09:48:40 -0700

    finished scrapping web archive

--- scrape_old_essenceRO_itemdb_web_archive.py
+++ scrape_old_essenceRO_itemdb_web_archive.py
@@ -34,10 +34,10 @@ else:
     # if file does not exist create a new one in write mode
     print("Creating a new file...")
     f = open(file=filename, mode="w")
-    header = "Item ID,For Sale,Identifier,Credit Price,Name,Type,NPC Buy," \
-             "Weight,NPC Sell,Attack,Range,Defense,Slots,Refineable,Equip Level," \
-             "Weapon Level,Equip,Locations,Equip Upper,Equippable Jobs,Equip Gender," \
-             "Item Use Script,Equip Script,Unequip Script,Image Path,Icon Path\n"
+    header = "Item ID\tFor Sale\tIdentifier\tCredit Price\tName\tType\tNPC Buy\t" \
+             "Weight\tNPC Sell\tAttack\tRange\tDefense\tSlots\tRefineable\tEquip Level\t" \
+             "Weapon Level\tEquip\tLocations\tEquip Upper\tEquippable Jobs\tEquip Gender\t" \
+             "Item Use Script\tEquip Script\tUnequip Script\tImage Path\tIcon Path\n"
     f.write(header)
     print("Added header...")
 
@@ -45,10 +45,10 @@ root_url = "https://web.archive.org/web/20150524022657/http://essence-ro.com/ite
 
 # start at id 20000 if non exists, otherwise
 # start at the last id pare
-if len(existing_ids) > 0:
-    beg = int(existing_ids[-1]) + 1
-else:
-    beg = 20000
+# if len(existing_ids) > 0:
+#     beg = int(existing_ids[-1]) + 1
+# else:
+#     beg = 20000
 
 # turn on the web client, must be downloaded from:
 # https://sites.google.com/a/chromium.org/chromedriver/downloads
@@ -57,75 +57,78 @@ chrome_driver_path = "D:/repos/scrape_eRO/chromedriver.exe"
 browser = webdriver.Chrome(executable_path=chrome_driver_path)
 
 # loops between item id #20000 and #32022
-for i in range(beg, 32022):
-    item_id = i
+for i in range(20000, -1, -1):
+    if(str(i) not in existing_ids):
+        item_id = i
 
-    target_url = root_url + "?id=" + str(item_id)
+        target_url = root_url + "?id=" + str(item_id)
 
-    # tells browser to navigate to page
-    browser.get(target_url)
+        # tells browser to navigate to page
+        browser.get(target_url)
 
-    # find all tables
-    query = browser.find_elements_by_tag_name("tbody")
+        # find all tables
+        query = browser.find_elements_by_tag_name("tbody")
 
-    # get last table
-    try:
-        tbody = query[5]
-        # find headers and values
-        tds = tbody.find_elements_by_tag_name("td")
-        ths = tbody.find_elements_by_tag_name("th")
+        # get last table
+        try:
+            tbody = query[5]
+            # find headers and values
+            tds = tbody.find_elements_by_tag_name("td")
+            ths = tbody.find_elements_by_tag_name("th")
 
-        item_entry = {}
-        for i in range(0, len(ths)):
-            header = ths[i].text
-            j = i
-            if(j > 0):
-                j = i + 1
-            value = tds[j].text
-            print(header + ": " + value)
-            item_entry[header] = value
-        # Grabs image path
-        image_path = tds[1].find_element_by_tag_name("img").get_attribute("src")
-        item_entry["Image Path"] = image_path
-        print("Image Path: " + image_path)
-        # Grabs icon path
-        icon_path = browser.find_element_by_tag_name("h3").find_element_by_tag_name("img").get_attribute("src")
-        item_entry["Icon Path"] = icon_path
-        print("Icon Path: " + icon_path)
-        entry_list = [
-            item_entry["Item ID"],
-            item_entry["For Sale"],
-            item_entry["Identifier"],
-            item_entry["Credit Price"],
-            item_entry["Name"],
-            item_entry["Type"],
-            item_entry["NPC Buy"],
-            item_entry["Weight"],
-            item_entry["NPC Sell"],
-            item_entry["Attack"],
-            item_entry["Range"],
-            item_entry["Defense"],
-            item_entry["Slots"],
-            item_entry["Refineable"],
-            item_entry["Equip Level"],
-            item_entry["Weapon Level"],
-            item_entry["Equip Locations"],
-            item_entry["Equip Upper"],
-            item_entry["Equippable Jobs"],
-            item_entry["Equip Gender"],
-            item_entry["Item Use Script"],
-            item_entry["Equip Script"],
-            item_entry["Unequip Script"],
-            item_entry["Image Path"],
-            item_entry["Icon Path"]
-        ]
-        line = "\t".join(entry_list)
-        line += "\n"
-        print("Writing item file...")
-        f.write(line)
-    except IndexError:
-        print("Skipping Item Id: " + str(item_id))
-        line = str(item_id) + "".join(["\t" * 24]) + "\n"
-        f.write(line)
+            item_entry = {}
+            for i in range(0, len(ths)):
+                header = ths[i].text
+                j = i
+                if(j > 0):
+                    j = i + 1
+                value = tds[j].text
+                print(header + ": " + value)
+                item_entry[header] = value
+            # Grabs image path
+            image_path = tds[1].find_element_by_tag_name("img").get_attribute("src")
+            item_entry["Image Path"] = image_path
+            print("Image Path: " + image_path)
+            # Grabs icon path
+            icon_path = browser.find_element_by_tag_name("h3").find_element_by_tag_name("img").get_attribute("src")
+            item_entry["Icon Path"] = icon_path
+            print("Icon Path: " + icon_path)
+            entry_list = [
+                item_entry["Item ID"],
+                item_entry["For Sale"],
+                item_entry["Identifier"],
+                item_entry["Credit Price"],
+                item_entry["Name"],
+                item_entry["Type"],
+                item_entry["NPC Buy"],
+                item_entry["Weight"],
+                item_entry["NPC Sell"],
+                item_entry["Attack"],
+                item_entry["Range"],
+                item_entry["Defense"],
+                item_entry["Slots"],
+                item_entry["Refineable"],
+                item_entry["Equip Level"],
+                item_entry["Weapon Level"],
+                item_entry["Equip Locations"],
+                item_entry["Equip Upper"],
+                item_entry["Equippable Jobs"],
+                item_entry["Equip Gender"],
+                item_entry["Item Use Script"],
+                item_entry["Equip Script"],
+                item_entry["Unequip Script"],
+                item_entry["Image Path"],
+                item_entry["Icon Path"]
+            ]
+            line = "\t".join(entry_list)
+            line += "\n"
+            print("Writing item file...")
+            f.write(line)
+        except IndexError:
+            print("Skipping Item Id: " + str(item_id))
+            line = str(item_id) + "".join(["\t" * 24]) + "\n"
+            f.write(line)
+    else:
+        print("Item Id #" + str(i) + " already exists. Skipping...")
 
 f.close()
