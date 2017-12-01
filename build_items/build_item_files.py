c34686533ce9160d7ac076c636d6bfc3b1f24386
Phuc H Duong <phuchduong@hotmail.com>
2017-12-01 14:20:00 -0700

    wrote a function that parses in item_dbs

--- build_items/build_item_files.py
+++ build_items/build_item_files.py
@@ -32,7 +32,7 @@ def main():
     #       /old_essence_item_db.txt
     #   \web_scrape_web_archive
     #       /item_db_web_archive.tsv
-    if isdir("C:/repos/essencero_restoration"):
+    if isdir("C:/repos"):
         repo_dir = "C:/repos"
     elif isdir("D:/repos"):
         repo_dir = "D:/repos"  # change this to your own
@@ -42,12 +42,6 @@ def main():
 
     # data webscraped from web archive (the way back machine)
     wa_dir = repo_dir + "/essencero_restoration/web_scrape_web_archive/item_db_web_archive.tsv"
-
-    # new iteminfo
-    new_iteminfo_dir = repo_dir + "/eRODev/eRO Client Data\data/luafiles514/lua files\datainfo/pre_re_itemInfo.lua"
-
-    # old iteminfo
-    old_iteminfo_dir = repo_dir + "/eRODev/eRO Client Data\system/itemInfosryx.lub"
 
     ##################
     # old eRO fields #
@@ -85,6 +79,8 @@ def main():
         20581,  # Drooping Zhao
     ]
 
+    master_db = {}
+
     ######################
     # tamsinwhitfield db #
     ######################
@@ -106,6 +102,12 @@ def main():
         wa_header = wa.readline()  # header
         wa_dict = parse_item_scrape_tsv(file_reader=wa, ignore_list=old_ero_ignore_list)
         print("Found... " + str(len(wa_dict)) + " items...")
+
+    #########################
+    # reborn item_db custom #
+    #########################
+    reborn_item_db_dir = repo_dir + "/eRODev/rAthena Files/db/import/ero_item_db/item_db.txt"
+    master_db = parse_item_db(file_dir=reborn_item_db_dir, item_db=master_db)
 
     ##################################################
     # Combine both old item db dictionaries together #
@@ -136,22 +138,40 @@ def main():
     # 437 is english codec
     encoding = "437"
 
+    ########################
+    # reborn item_info lua #
+    ########################
     # parses ero iteminfo lua file into a python dictionary
-    old_item_info_lua_renewal = repo_dir + "/eRODev/eRO Client Data\system/itemInfosryx.lub"
-    old_ero_lua = parse_item_info_lua(file_dir=old_item_info_lua_renewal, encoding=encoding)
-    # old_ero_headers = scan_headers(dictionary=old_ero_lua["mid"], prefix='o', pk="reborn_id")
+    existing_reborn_lua_dir = repo_dir + "/eRODev/eRO Client Data/system/itemInfosryx.lub"
+    existing_reborn_lua = parse_item_info_lua(file_dir=existing_reborn_lua_dir, encoding=encoding)
+    existing_reborn_lua_headers = scan_headers(dictionary=existing_reborn_lua["mid"], name_of_pk="reborn_id")
 
-    # parses pre-renewal iteminfo lua file into a python dictionary
-    old_item_info_lua_prerenewal = repo_dir + "/eRODev/eRO Client Data\data/luafiles514/lua files\datainfo/pre_re_itemInfo.lua"
-    prerenewal_lua = parse_item_info_lua(file_dir=old_item_info_lua_prerenewal, encoding=encoding)
+    #############################
+    # pre-renewal item_info lua #
+    #############################
+    # # parses pre-renewal iteminfo lua file into a python dictionary
+    # old_item_info_lua_prerenewal = repo_dir + "/eRODev/eRO Client Data/data/luafiles514/lua files/datainfo/pre_re_itemInfo.lua"
+    # prerenewal_lua = parse_item_info_lua(file_dir=old_item_info_lua_prerenewal, encoding=encoding)
+    # # sees if the new lua has anythign that the old lua lacks
+    # for item_id in prerenewal_lua["mid"]:
+    #     if item_id not in existing_reborn_lua["mid"]:
+    #         print("base lua missing item id: " + str(item_id))
 
-    for item_id in prerenewal_lua["mid"]:
-        if item_id not in old_ero_lua["mid"]:
-            print("item id: " + str(item_id))
+    ###########
+    # outputs #
+    ###########
+    write_master_db_to_tsv(file_dir=repo_dir + "/eRODev/work in progress/master_db.tsv")
+
+    # writes the item lua to a tsv
+    write_lua_items_to_tsv(
+        file_dir=repo_dir + "/eRODev/eRO Client Data/System/new_itemInfosryx.tsv",
+        headers=existing_reborn_lua_headers,
+        lua_dict=existing_reborn_lua["mid"],
+        encoding=encoding)
 
     # Writes new lua to file
-    new_item_info_lua = repo_dir + "/eRODev/eRO Client Data/system/new_itemInfosryx.lub"
-    write_item_info_lua_to_file(file_dir=new_item_info_lua, lua_parts=old_ero_lua, encoding=encoding)
+    new_lua_dir = repo_dir + "/eRODev/eRO Client Data/system/new_itemInfosryx.lub"
+    write_lua_items_to_lua(file_dir=new_lua_dir, lua_parts=existing_reborn_lua, encoding=encoding)
 
 
 # Parses a TSV and returns a dictionary.
@@ -183,10 +203,6 @@ def parse_item_scrape_tsv(file_reader, ignore_list):
 # mid is the data structure in the middle of the file as a dictionary
 # end is the remaining code in the file after the data structure
 def parse_item_info_lua(file_dir, encoding):
-    # if is_korean:
-    #     encoding = "ms949"
-    # else:
-    #     encoding = "UTF8"
     ############
     # Patterns #
     ############
@@ -285,6 +301,71 @@ def parse_item_info_lua(file_dir, encoding):
     return item_lua_dict
 
 
+# Takes in an existing item dictionary then adds to it by reading in an item_db
+# Parameters:
+#   file_dir = full path to the item_db.txt file
+#   item_db = dictionary of existing items
+# Return: the same item_db that was augmented with the item_db entries
+def parse_item_db(file_dir, item_db):
+    with open(file=file_dir, mode="r") as reborn_item_db_f:
+        item_db_entry_pattern = '^\d{3,5}(,.{0,}){19,}$\n'  # looks for an item_id followed by 19 commas
+        is_item_db_line = re.compile(item_db_entry_pattern)
+        counter = 0
+        for line in reborn_item_db_f:
+            if is_item_db_line.match(line):
+                item_n_scripts = line.split(",{")
+                attribute_line = item_n_scripts[0]
+                attributes = attribute_line.split(",")
+
+                item_id = attributes[0]
+                if item_id not in item_db:
+                    item_db[item_id] = {}
+                item_db[item_id]["display_name"] = attributes[2].strip()
+                item_db[item_id]["item_type"] = attributes[3].strip()
+                item_db[item_id]["buy_price"] = attributes[4].strip()
+                item_db[item_id]["weight"] = attributes[6].strip()
+                item_db[item_id]["weapon_atk"] = attributes[7].strip()
+                item_db[item_id]["defense"] = attributes[8].strip()
+                item_db[item_id]["weapon_range"] = attributes[9].strip()
+                item_db[item_id]["slots"] = attributes[10].strip()
+                item_db[item_id]["job"] = attributes[11].strip()
+                item_db[item_id]["upper"] = attributes[12].strip()
+                item_db[item_id]["gender"] = attributes[13].strip()
+                item_db[item_id]["loc"] = attributes[14].strip()
+                item_db[item_id]["weapon_lvl"] = attributes[15].strip()
+                item_db[item_id]["required_lvl"] = attributes[16].strip()
+                item_db[item_id]["refineable_lvl"] = attributes[17].strip()
+                item_db[item_id]["view_id"] = attributes[18].strip()
+                item_db[item_id]["script"] = item_n_scripts[1].replace("}", "").strip()
+                item_db[item_id]["script_on_equip"] = item_n_scripts[2].replace("}", "").strip()
+                item_db[item_id]["script_on_unequip"] = item_n_scripts[3].replace("}", "").strip()
+
+                counter += 0
+    print("Found " + str(counter) + " in " + file_dir)
+    return item_db
+
+
+# Summary: takes in a lua dictionary, flattens it and outputs to csv
+# Params:
+#       -file_dir: full path to file to be written out
+#       -headers: the headers that will exist in this lua file, also to used as keys to traverse
+#           the lua dictionary.
+#       -lua_dict: the dictionary to be converted to csv
+def write_lua_items_to_tsv(file_dir, headers, lua_dict, encoding):
+    f = open(file=file_dir, mode="w", encoding=encoding)
+    f.write("\t".join(headers) + "\n")   # writes headers
+    counter = 1
+    for item_id in lua_dict:    # loops over each item
+        line = str(item_id)
+        for header in headers:  # loops over each attribute inside each item
+            if header in lua_dict[item_id]:
+                line += "\t" + str(lua_dict[item_id][header])
+        f.write(line + "\n")
+        counter += 1
+    f.close()
+    print("Wrote " + str(counter) + " items to: " + file_dir)
+
+
 # Writes an iteminfo lua and from a 3 element dictionary parameter [1]<lua_parts> whoses keys are
 # beg, mid, and end.
 # beg is the begining of the file
@@ -293,7 +374,7 @@ def parse_item_info_lua(file_dir, encoding):
 # [0]<file_dir> is the full path of the file to be written
 # [2]<is_korean> can be True or False, true will specify encoding of ms949 for korean encoding
 #     while false will yeild utf-8 encoding
-def write_item_info_lua_to_file(file_dir, lua_parts, encoding):
+def write_lua_items_to_lua(file_dir, lua_parts, encoding):
     # file parts
     lua_beg = lua_parts["beg"]
     lua_dict = lua_parts["mid"]
@@ -303,11 +384,6 @@ def write_item_info_lua_to_file(file_dir, lua_parts, encoding):
     spaces_per_tab = 4
     tab = " " * spaces_per_tab
 
-    # # opening file for write
-    # if is_korean:
-    #     encoding = "ms949"
-    # else:
-    #     encoding = "utf-8"
     f = open(file=file_dir, mode="w+", encoding=encoding)
     f.write(lua_beg)  # first line
 
@@ -341,12 +417,13 @@ def write_item_info_lua_to_file(file_dir, lua_parts, encoding):
 #       merging two dictionaries together
 #   pk: primary key name of the parent key of the file to be incorporated into the list of header names
 # Return: a list of header names
-def scan_headers(dictionary, prefix, pk):
-    headers = [pk]
-    for key in dictionary:
-        if headers[key]:
-            if key not in headers:
-                headers.append(key)
+def scan_headers(dictionary, name_of_pk):
+    headers = [name_of_pk]
+    for item_id in dictionary:  # loop over all items
+        for attribute in dictionary[item_id]:  # loop over all attributes of items
+            if attribute not in headers:
+                headers.append(attribute)
+    return headers
 
 
 main()
