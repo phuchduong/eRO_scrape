c13b7432d4daf43bc0bce76c64a26111a0be49ca
Phuc H Duong <phuchduong@hotmail.com>
2017-12-03 02:28:43 -0700

    fixed a bug that broke one line descriptions

--- build_items/insert_new_items_from_itemdb_into_iteminfo.py
+++ build_items/insert_new_items_from_itemdb_into_iteminfo.py
@@ -220,7 +220,7 @@ def write_lua_items_to_lua(file_dir, lua_parts, encoding):
             if isinstance(lua_dict[item_id][item_key], list):
                 multi_line_embed_str = tab * 2 + str(item_key) + " = {\n"
 
-                if len(lua_dict[item_id][item_key]) > 1:
+                if len(lua_dict[item_id][item_key]) >= 1:
                     for item in lua_dict[item_id][item_key]:
                         multi_line_embed_str += tab * 3 + item + ",\n"
                 else:
