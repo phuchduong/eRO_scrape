9d0b01a7944138fe91af06d737efe766b15fead7
Phuc H Duong <phuchduong@hotmail.com>
2018-01-31 05:26:20 -0700

    Parse item_info

--- scripts/print_item_names.py
+++ scripts/print_item_names.py
@@ -3,7 +3,7 @@
     File name: print_tem_names.py
     Date created: January 31, 2017
     Python version: 3.6.1
-    Version: 0.2.0
+    Version: 0.3.0
     Purpose:
         Prints the item names from an item_db and an item_info.
     Author: Phuc H Duong
@@ -11,13 +11,13 @@
     Website: phuchduong.io
     Linkedin: https://www.linkedin.com/in/phuchduong/
 '''
-import os   # checks to see if a folder exists
 from os import path, makedirs
 import re  # regular expression
 
 
 # script goes here
 def main():
+    debug_mode = True
     # Repo folder
     if path.isdir("C:/repos"):
         repo_dir = "C:/repos"
@@ -41,26 +41,21 @@ def main():
     # Output files
     out_filename = "item_names.tsv"
 
-    item_db = parse_item_names_from_item_db(db_path=item_db)
-
-
-# Traveres an item_db.txt and gets all item_ids and item names.
-def parse_item_names_from_item_db(db_path):
-    item_regex = "^\d{3,5},"
-    item_db = {}
-    is_item = re.compile(item_regex)
-    with open(file=db_path, mode="r") as f:
-        for line in f:
-            if is_item.match(line):
-                print(line)
-    return item_db
+    item_db_names = parse_item_names_from_item_db(
+        db_path=item_db,
+        debug=debug_mode
+    )
+    item_info_namse = parse_item_names_from_item_info(
+        info_path=item_info,
+        debug=debug_mode
+    )
 
 
 # Loads the local file system, else create a new one.
 def make_output_folder():
     # Requires import os
     # Get the current file path of the script.
-    script_dir = path.dirname(os.path.realpath(__file__))
+    script_dir = path.dirname(path.realpath(__file__))
     file_system_path = script_dir + "/outputs/"
     system_folder_exists = path.isdir(file_system_path)
 
@@ -74,6 +69,50 @@ def make_output_folder():
         print("Created folder: " + file_system_path)
 
     return(file_system_path)
+
+
+# Traveres an item_db.txt and gets all item_ids and item names.
+def parse_item_names_from_item_db(db_path, debug):
+    item_regex = "^\d{3,5},"
+    item_db = {}
+    is_item = re.compile(item_regex)
+    with open(file=db_path, mode="r") as f:
+        for line in f:
+            if is_item.match(line):
+                line_split = line.split(",")
+                item_id = int(line_split[0])
+                aegis_name = line_split[1]
+                rathena_name = line_split[2]
+                item_db[item_id] = {
+                    "aegis_name": aegis_name,
+                    "rathena_name": rathena_name
+                }
+                if debug:
+                    print(str(item_id) + "\t" + aegis_name + "\t" + rathena_name)
+    return item_db
+
+
+# Traveres an item_db.txt and gets all item_ids and item names.
+def parse_item_names_from_item_info(info_path, debug):
+    item_id_regex = "\[\d{3,5}\]"
+    item_display_regex = "^\s{1,}identifiedDisplayName"
+    is_item_id = re.compile(item_id_regex)
+    is_item_display = re.compile(item_display_regex)
+    item_info = {}
+    current_id = None
+    with open(file=info_path, mode="r", encoding="850") as f:
+        for line in f:
+            if is_item_id.search(line):
+                current_id = int(line.split("[")[1].split("]")[0])
+                item_info[current_id] = {}
+            if is_item_display.search(line):
+                line_split = line.split("=")
+                display_name = line_split[1].strip()
+                display_name = display_name.split("\"")[1].strip()
+                item_info[current_id]["display_name"] = display_name
+                if debug:
+                    print(str(current_id) + "\t" + item_info[current_id]["display_name"])
+    return item_info
 
 
 main()
