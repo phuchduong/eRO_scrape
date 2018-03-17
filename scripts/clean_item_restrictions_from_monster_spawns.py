c781378bf10dbf5d813d3f0b8ea249e356ca81e9
Phuc H Duong <phuchduong@hotmail.com>
2018-03-17 10:29:58 -0700

    Initialization of the cleanup script.

---
+++ scripts/clean_item_restrictions_from_monster_spawns.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python.
+'''
+    File name: clean_item_restrictions_from_monster_spawns.py
+    Date created: March 17, 2018
+    Python version: 3.6.1
+    Version: 0.1.0
+    Purpose:
+        Goal: Fix, the item_trade.txt
+            • Get a list of monsters that naturally spawn.
+            • Link those monsters that spawn, with their drops.
+            • Get a list of items that are dropped by monsters that naturally spawn
+            • Scan through, the item_trade.txt
+            • Find items, that are dropped by monsters that spawn naturally,
+            • Then: remove those items.
+    Author: Phuc H Duong
+    Original Repo: https://github.com/phuchduong/essencero_restoration
+    Website: phuchduong.io
+    Linkedin: https://www.linkedin.com/in/phuchduong/
+'''
